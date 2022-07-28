42b4acfd35154157ca7ef44f689834d2dff9d298
error4767 <2303184060@qq.com>
2022-07-28 19:29:48 +0800

    clipboard serve are receive body content

--- src/clipboard_serve.rs
+++ src/clipboard_serve.rs
@@ -4,45 +4,33 @@ use actix_web::{
   error,
   Error,
   web,
-  HttpRequest, 
   HttpResponse,
 };
 
+use futures::StreamExt;
+
 use async_std::fs;
-
-use actix_utils::{
-  get_header
-};
-
-use urlencoding::decode;
 
 #[post("/cloud_clipboard/add/{uid}")]
 async fn cloud_clipboard_add(
-  req: HttpRequest,
-  web::Path(uid): web::Path<String>
+  web::Path(uid): web::Path<String>,
+  mut payload: web::Payload,
 ) -> Result<HttpResponse, Error> {
-  match get_header(&req, "textContent") {
-    Some(raw_text_content)=> {
-      let text_content = String::from(match decode(raw_text_content) {
-        Ok(text_content) => text_content,
-        Err(_) => {
-          // 文本格式不对，不符合url编码
-          return Err(error::ErrorBadRequest("failed to parse request header textContent"));
-        }
-      });
-      let file_path = format!("./clipboard/{}.txt", uid);
-      if let Err(_) = fs::File::create(&file_path).await {
-        return Err(error::ErrorBadRequest("failed to create file"));
-      };
-      if let Err(_) = fs::write(&file_path, &text_content).await {
-        return Err(error::ErrorBadRequest("failed to write file"));
-      };
-      Ok(HttpResponse::Ok().body(""))
-    },
-    None=> {
-      return Err(error::ErrorBadRequest("request header textContent is not found"));
-    }
-  }
+  // 取得请求体
+  let mut text_content = web::BytesMut::new();
+  while let Some(chunk) = payload.next().await {
+    let chunk = chunk?;
+    text_content.extend_from_slice(&chunk);
+  };
+  // 写入内容
+  let file_path = format!("./clipboard/{}.txt", uid);
+  if let Err(_) = fs::File::create(&file_path).await {
+    return Err(error::ErrorBadRequest("failed to create file"));
+  };
+  if let Err(_) = fs::write(&file_path, &text_content).await {
+    return Err(error::ErrorBadRequest("failed to write file"));
+  };
+  Ok(HttpResponse::Ok().body(""))
 }
 
 #[get("/cloud_clipboard/get/{uid}")]
